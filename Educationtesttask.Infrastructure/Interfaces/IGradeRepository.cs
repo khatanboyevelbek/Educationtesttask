be2718967102b975cb224ae3aef46391726b09e9
Elbek Khatanboyev <programmerelbek@gmail.com>
2023-06-17 11:36:30 +0500

    Implament Grade Repository

---
+++ Educationtesttask.Infrastructure/Interfaces/IGradeRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Educationtesttask.Domain.Entities;
+
+namespace Educationtesttask.Infrastructure.Interfaces
+{
+	public interface IGradeRepository : IGenericRepository<Grade>
+	{
+	}
+}
---
+++ Educationtesttask.Infrastructure/Repositories/GradeRepository.cs
@@ -0,0 +1,13 @@
+﻿using Educationtesttask.Domain.Entities;
+using Educationtesttask.Infrastructure.Data;
+using Educationtesttask.Infrastructure.Interfaces;
+
+namespace Educationtesttask.Infrastructure.Repositories
+{
+	public class GradeRepository : GenericRepository<Grade>, IGradeRepository
+	{
+		public GradeRepository(AppDbContext appDbContext) 
+			: base(appDbContext)
+		{ }
+	}
+}
