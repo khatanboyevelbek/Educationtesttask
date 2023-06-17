100dc6087f2cf6d52cd9e8af5a8ca6379dc9ca78
Elbek Khatanboyev <programmerelbek@gmail.com>
2023-06-17 11:30:26 +0500

    Implament Student Repository

---
+++ Educationtesttask.Infrastructure/Repositories/StudentRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Educationtesttask.Domain.Entities;
+using Educationtesttask.Infrastructure.Data;
+using Educationtesttask.Infrastructure.Interfaces;
+
+namespace Educationtesttask.Infrastructure.Repositories
+{
+	public class StudentRepository : GenericRepository<Student>, IStudentRepository
+	{
+		public StudentRepository(AppDbContext appDbContext)
+			: base(appDbContext)
+		{ }
+	}
+}
