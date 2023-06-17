c12c81e760af7b0896476878e0e5c1600aff60ad
Elbek Khatanboyev <programmerelbek@gmail.com>
2023-06-17 11:46:12 +0500

    Implament Subject Repository

---
+++ Educationtesttask.Infrastructure/Repositories/SubjectRepository.cs
@@ -0,0 +1,13 @@
+﻿using Educationtesttask.Domain.Entities;
+using Educationtesttask.Infrastructure.Data;
+using Educationtesttask.Infrastructure.Interfaces;
+
+namespace Educationtesttask.Infrastructure.Repositories
+{
+	public class SubjectRepository : GenericRepository<Subject>, ISubjectRepository
+	{
+		public SubjectRepository(AppDbContext appDbContext) 
+			: base(appDbContext) 
+		{ }
+	}
+}
