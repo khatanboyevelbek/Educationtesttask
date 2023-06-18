f40c290c8be59761fc2182040ad5472464d99b37
Elbek Khatanboyev <programmerelbek@gmail.com>
2023-06-18 16:43:58 +0500

    Implament TeacheViewModel validation

---
+++ Educationtesttask.Application/Validations/TeacherViewModelValidation.cs
@@ -0,0 +1,17 @@
+﻿using Educationtesttask.Application.ViewModels;
+using FluentValidation;
+
+namespace Educationtesttask.Application.Validations
+{
+	public class TeacherViewModelValidation : AbstractValidator<TeacherViewModel>
+	{
+		public TeacherViewModelValidation() 
+		{
+			RuleFor(tvm => tvm.FirstName).NotNull().NotEmpty();
+			RuleFor(tvm => tvm.LastName).NotNull().NotEmpty();
+			RuleFor(tvm => tvm.PhoneNumber).NotNull().NotEmpty().Must(n => n.StartsWith("+998"));
+			RuleFor(tvm => tvm.Email).NotNull().NotEmpty().EmailAddress();
+			RuleFor(tvm => tvm.BirthDate).NotNull().NotEmpty();
+		}
+	}
+}
