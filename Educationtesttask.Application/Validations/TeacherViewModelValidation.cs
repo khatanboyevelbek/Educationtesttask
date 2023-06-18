710fa2ef56605961524ad749d4e8cf469bd9265d
Elbek Khatanboyev <programmerelbek@gmail.com>
2023-06-18 16:57:20 +0500

    Add validation error message

--- Educationtesttask.Application/Validations/TeacherViewModelValidation.cs
+++ Educationtesttask.Application/Validations/TeacherViewModelValidation.cs
@@ -7,11 +7,21 @@ namespace Educationtesttask.Application.Validations
 	{
 		public TeacherViewModelValidation() 
 		{
-			RuleFor(tvm => tvm.FirstName).NotNull().NotEmpty();
-			RuleFor(tvm => tvm.LastName).NotNull().NotEmpty();
-			RuleFor(tvm => tvm.PhoneNumber).NotNull().NotEmpty().Must(n => n.StartsWith("+998"));
-			RuleFor(tvm => tvm.Email).NotNull().NotEmpty().EmailAddress();
-			RuleFor(tvm => tvm.BirthDate).NotNull().NotEmpty();
+			RuleFor(tvm => tvm.FirstName).NotNull().NotEmpty()
+				.WithMessage("Please provide a valid firstname");
+
+			RuleFor(tvm => tvm.LastName).NotNull().NotEmpty()
+				.WithMessage("Please provide a valid lastname");
+
+			RuleFor(tvm => tvm.PhoneNumber).NotNull().NotEmpty()
+				.Must(n => n.StartsWith("+998"))
+				.WithMessage("Please provide a valid phone number starting with '+998'");
+
+			RuleFor(tvm => tvm.Email).NotNull().NotEmpty().EmailAddress()
+				.WithMessage("Please provide a valid email address");
+
+			RuleFor(tvm => tvm.BirthDate).NotNull().NotEmpty()
+				.WithMessage("Please provide a valid birth date");
 		}
 	}
 }
