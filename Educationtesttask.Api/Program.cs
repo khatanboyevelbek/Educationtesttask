3a2b045d6c659b0850458e24f12341e114b58250
Elbek Khatanboyev <programmerelbek@gmail.com>
2023-06-20 14:11:13 +0500

    Register Teacher Service

--- Educationtesttask.Api/Program.cs
+++ Educationtesttask.Api/Program.cs
@@ -1,4 +1,6 @@
+using Educationtesttask.Application.Interfaces;
 using Educationtesttask.Application.Logging;
+using Educationtesttask.Application.Services;
 using Educationtesttask.Application.Validations;
 using Educationtesttask.Infrastructure.Data;
 using Educationtesttask.Infrastructure.Interfaces;
@@ -23,6 +25,7 @@ namespace Educationtesttask.Api
 			RegisterDbContext(builder.Services, builder.Configuration);
 			RegisterRepositories(builder.Services);
 			RegisterUtilities(builder.Services);
+			RegisterServices(builder.Services);
 			
 
 			var app = builder.Build();
@@ -59,6 +62,10 @@ namespace Educationtesttask.Api
 		{
 			services.AddScoped<ISerilogLogger, SerilogLogger>();
 			services.AddTransient<TeacherViewModelValidation>();
+		}
+		private static void RegisterServices(IServiceCollection services)
+		{
+			services.AddTransient<ITeacherService, TeacherService>();
 		}
 	}
 }
